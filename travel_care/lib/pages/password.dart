f6925038d0b83bb3bb01200e06e1239d1d125cd3
Leonardo Maia <leof.maia88@gmail.com>
2023-11-01 20:50:10 -0300

    recuperação de senha no back4app

--- travel_care/lib/pages/password.dart
+++ travel_care/lib/pages/password.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:parse_server_sdk_flutter/parse_server_sdk_flutter.dart';
 import 'package:travel_care/helpers/validation_helper.dart';
 import 'package:travel_care/components/myDialog.dart';
+import 'package:travel_care/pages/login.dart';
 
 class PasswordPage extends StatefulWidget {
   const PasswordPage({super.key});
@@ -11,11 +13,14 @@ class PasswordPage extends StatefulWidget {
 
 class _PasswordPageState extends State<PasswordPage> {
   final _formKey = GlobalKey<FormState>();
+  final controllerEmail = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("Redefinir Senha"),),
+      appBar: AppBar(
+        title: const Text("Redefinir Senha"),
+      ),
       body: Padding(
         padding: const EdgeInsets.all(30.0),
         child: SingleChildScrollView(
@@ -50,6 +55,7 @@ class _PasswordPageState extends State<PasswordPage> {
                   ),
                   const SizedBox(height: 15),
                   TextFormField(
+                    controller: controllerEmail,
                     keyboardType: TextInputType.emailAddress,
                     decoration: const InputDecoration(
                       hintText: 'Ex: seuemail@email.com',
@@ -59,9 +65,7 @@ class _PasswordPageState extends State<PasswordPage> {
                       return validateEmptyField(text);
                     },
                   ),
-                                  
                   const SizedBox(height: 30),
-        
                   ElevatedButton(
                     onPressed: () => recuperarSenha(),
                     child: const Text(
@@ -79,17 +83,40 @@ class _PasswordPageState extends State<PasswordPage> {
       ),
     );
   }
-  void recuperarSenha() {
+
+  void recuperarSenha() async {
     if (!_formKey.currentState!.validate()) {
       return;
     }
-    showDialog(
-        context: context,
-        builder: (BuildContext context) {
-          return MyDialog(
-              "E-mail enviado com sucesso.", () => Navigator.of(context).pop());
-        });
+
+    var email = controllerEmail.text.trim();
+
+    // consultar se email existe
+    ParseUser user = ParseUser(null, null, email);
+    var response = await user.requestPasswordReset();
+
+    if (!context.mounted) {
+      return;
+    }
+
+    if (response.success) {
+      showDialog(
+          context: context,
+          builder: (BuildContext context) {
+            return MyDialog(
+                "E-mail enviado com sucesso.",
+                () => Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => const LoginPage())));
+          });
+    } else {
+      showDialog(
+          context: context,
+          builder: (BuildContext context) {
+            return MyDialog("Falha ao enviar email de redefinição de senha",
+                () => Navigator.of(context).pop());
+          });
+    }
   }
 }
-
-
