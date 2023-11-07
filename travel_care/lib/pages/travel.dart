3be740252f86a8d37224d44ddb7dd6f19f30e0f0
Guilherme Henrique Candido Moraes <guilherme.moraes@see.go.gov.br>
2023-11-07 18:39:22 -0300

    alterações em tela de viagem

--- travel_care/lib/pages/travel.dart
+++ travel_care/lib/pages/travel.dart
@@ -1,5 +1,5 @@
-
 import 'package:flutter/material.dart';
+import 'package:travel_care/components/myDialog.dart';
 import 'package:travel_care/pages/home.dart';
 
 class TravelPage extends StatefulWidget {
@@ -9,18 +9,23 @@ class TravelPage extends StatefulWidget {
 }
 
 class _TravelPageState extends State<TravelPage> {
-  //List cidades = ["Ceres", "Itapaci", "São Patrício", "Ceres", "Itapaci", "São Patrício"];
-  List cidades = ["Ceres"];
-
-
+  List cidades = [
+    "Ceres",
+    "Itapaci",
+    "São Patrício",
+    "Ceres",
+    "Itapaci",
+    "São Patrício"
+  ];
+  //List cidades = ["Ceres"];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Padding(
-        padding: const EdgeInsets.only(left: 20.0),
+        padding: const EdgeInsets.only(left: 20.0, right: 20),
         child: Column(children: [
-          const SizedBox(height: 30),
+          const SizedBox(height: 40),
           const Text(
             'Minhas Solicitações:',
             style: TextStyle(
@@ -39,77 +44,91 @@ class _TravelPageState extends State<TravelPage> {
                   shape: RoundedRectangleBorder(
                     borderRadius: BorderRadius.circular(10),
                   ),
-                  child: const Padding(
-                    padding: EdgeInsets.all(20.0),
-                    child: Column(
-                      children: <Widget>[
-                        Row(
-                          children: [
-                            Text(
-                              'Destino: ',
-                              style: TextStyle(
-                                color: Colors.black,
-                                fontSize: 18,
-                                fontWeight: FontWeight.bold,
-                              ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(20.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Row(
+                              children: [
+                                const Text(
+                                  'Destino: ',
+                                  style: TextStyle(
+                                    color: Colors.black,
+                                    fontSize: 18,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                Text(
+                                  cidades[index],
+                                  style: const TextStyle(
+                                    color: Colors.black,
+                                    fontSize: 18,
+                                  ),
+                                ),
+                              ],
                             ),
-                            Text(
-                              'Goiânia',
-                              style: TextStyle(
-                                color: Colors.black,
-                                fontSize: 18,
-                              ),
+                            const SizedBox(height: 10),
+                            const Row(
+                              children: [
+                                Text(
+                                  'Data: ',
+                                  style: TextStyle(
+                                    color: Colors.black,
+                                    fontSize: 18,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                Text(
+                                  '01/11/2023',
+                                  style: TextStyle(
+                                    color: Colors.black,
+                                    fontSize: 18,
+                                  ),
+                                ),
+                              ],
+                            ),
+                            const SizedBox(height: 10),
+                            const Row(
+                              children: [
+                                Text(
+                                  'Situação: ',
+                                  style: TextStyle(
+                                    color: Colors.black,
+                                    fontSize: 18,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                Text(
+                                  'Aceita',
+                                  style: TextStyle(
+                                    color: Colors.black,
+                                    fontSize: 18,
+                                  ),
+                                ),
+                              ],
                             ),
                           ],
                         ),
-                        SizedBox(height: 10),
-                        Row(
+                        Column(
                           children: [
-                            Text(
-                              'Data: ',
-                              style: TextStyle(
-                                color: Colors.black,
-                                fontSize: 18,
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
-                            Text(
-                              '01/11/2023',
-                              style: TextStyle(
-                                color: Colors.black,
-                                fontSize: 18,
-                              ),
+                            Row(
+                              children: [
+                                TextButton(
+                                  onPressed: () => info(),
+                                  child: const Icon(
+                                    Icons.more_horiz,
+                                    color: Colors.blue,
+                                    size: 65.0,
+                                    semanticLabel: 'Saiba Mais',
+                                  ),
+                                ),
+                              ],
                             ),
                           ],
-                        ),
-                        SizedBox(height: 10),
-                        Row(
-                          children: [
-                            Text(
-                              'Situação: ',
-                              style: TextStyle(
-                                color: Colors.black,
-                                fontSize: 18,
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
-                            Text(
-                              'Aceita',
-                              style: TextStyle(
-                                color: Colors.black,
-                                fontSize: 18,
-                              ),
-                            ),
-                          ],
-                        ),
-                        TextButton(
-                          onPressed: info(),
-                          child: Text(
-                            '...',
-                            style: TextStyle(
-                              fontSize: 22,
-                            ),
-                          ),
                         ),
                       ],
                     ),
@@ -119,7 +138,7 @@ class _TravelPageState extends State<TravelPage> {
             ),
           ),
           Padding(
-            padding: const EdgeInsets.only(right: 170),
+            padding: const EdgeInsets.only(right: 170, bottom: 25),
             child: ElevatedButton(
                 onPressed: () => solicitar(),
                 style: ButtonStyle(
@@ -136,15 +155,89 @@ class _TravelPageState extends State<TravelPage> {
       ),
     );
   }
-    void info() {
-    Navigator.push(
-      context, MaterialPageRoute(builder: (context) => const HomePage()));
-}
+
+  info() {
+    showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            content: const Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Row(
+                  children: [
+                    Text(
+                      'Paciente: ',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Text(
+                      "Leonardo",
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 18,
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 10),
+                Row(
+                  children: [
+                    Text(
+                      'Acompanhante: ',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Text(
+                      'Não',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 18,
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 10),
+                Row(
+                  children: [
+                    Text(
+                      'Destino: ',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Text(
+                      'Goiânia',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 18,
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+            actions: <Widget>[
+              Center(
+                  child: ElevatedButton(
+                onPressed: () => Navigator.of(context).pop(),
+                child: const Text("Voltar"),
+              ))
+            ],
+          );
+        });
+  }
+
   void solicitar() {
     Navigator.push(
-      context, MaterialPageRoute(builder: (context) => const HomePage()));
+        context, MaterialPageRoute(builder: (context) => const HomePage()));
+  }
 }
-}
-
-
-
