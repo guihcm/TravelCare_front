a7164acfe234b4ab8a89e51b951fdcb870916e36
Guilherme Henrique Candido Moraes <guilherme.moraes@see.go.gov.br>
2023-11-07 15:54:06 -0300

    alterações

--- travel_care/lib/pages/travel.dart
+++ travel_care/lib/pages/travel.dart
@@ -1,15 +1,24 @@
+
 import 'package:flutter/material.dart';
+import 'package:travel_care/pages/home.dart';
 
-class TravelPage extends StatelessWidget {
-  TravelPage({super.key});
+class TravelPage extends StatefulWidget {
+  const TravelPage({super.key});
+  @override
+  State<TravelPage> createState() => _TravelPageState();
+}
 
-  List cidades = ["Ceres", "Itapaci", "São Patrício", "Ceres", "Itapaci", "São Patrício"];
+class _TravelPageState extends State<TravelPage> {
+  //List cidades = ["Ceres", "Itapaci", "São Patrício", "Ceres", "Itapaci", "São Patrício"];
+  List cidades = ["Ceres"];
+
+
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Padding(
-        padding: const EdgeInsets.all(30.0),
+        padding: const EdgeInsets.all(20.0),
         child: Column(children: [
           const SizedBox(height: 25),
           const Center(
@@ -24,83 +33,119 @@ class TravelPage extends StatelessWidget {
           ),
           const SizedBox(height: 25),
           Expanded(
-            child: ListView.builder(
-              itemCount: cidades.length,
-              itemBuilder: (context, index) => Card(
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(10),
-                ),
-                child: const Padding(
-                  padding: EdgeInsets.all(20.0),
-                  child: Column(
-                    children: <Widget>[
-                      Row(
-                        children: [
-                          Text(
-                            'Destino: ',
+            child: Padding(
+              padding: const EdgeInsets.only(bottom: 20),
+              child: ListView.builder(
+                itemCount: cidades.length,
+                itemBuilder: (context, index) => Card(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: const Padding(
+                    padding: EdgeInsets.all(20.0),
+                    child: Column(
+                      children: <Widget>[
+                        
+                        
+                        Row(
+                          children: [
+                            Text(
+                              'Destino: ',
+                              style: TextStyle(
+                                color: Colors.black,
+                                fontSize: 18,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            Text(
+                              'Goiânia',
+                              style: TextStyle(
+                                color: Colors.black,
+                                fontSize: 18,
+                              ),
+                            ),
+                          ],
+                        ),
+                        SizedBox(height: 10),
+                        Row(
+                          children: [
+                            Text(
+                              'Data: ',
+                              style: TextStyle(
+                                color: Colors.black,
+                                fontSize: 18,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            Text(
+                              '01/11/2023',
+                              style: TextStyle(
+                                color: Colors.black,
+                                fontSize: 18,
+                              ),
+                            ),
+                          ],
+                        ),
+                        SizedBox(height: 10),
+                        Row(
+                          children: [
+                            Text(
+                              'Situação: ',
+                              style: TextStyle(
+                                color: Colors.black,
+                                fontSize: 18,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            Text(
+                              'Aceita',
+                              style: TextStyle(
+                                color: Colors.black,
+                                fontSize: 18,
+                              ),
+                            ),
+                          ],
+                        ),
+                        TextButton(
+                          onPressed: info(),
+                          child: Text(
+                            '...',
                             style: TextStyle(
-                              color: Colors.black,
-                              fontSize: 18,
-                              fontWeight: FontWeight.bold,
+                              fontSize: 22,
                             ),
                           ),
-                          Text(
-                            'Goiânia',
-                            style: TextStyle(
-                              color: Colors.black,
-                              fontSize: 18,
-                            ),
-                          ),
-                        ],
-                      ),
-                      SizedBox(height: 10),
-                      Row(
-                        children: [
-                          Text(
-                            'Data: ',
-                            style: TextStyle(
-                              color: Colors.black,
-                              fontSize: 18,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                          Text(
-                            '01/11/2023',
-                            style: TextStyle(
-                              color: Colors.black,
-                              fontSize: 18,
-                            ),
-                          ),
-                        ],
-                      ),
-                      SizedBox(height: 10),
-                      Row(
-                        children: [
-                          Text(
-                            'Situação: ',
-                            style: TextStyle(
-                              color: Colors.black,
-                              fontSize: 18,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                          Text(
-                            'Aceita',
-                            style: TextStyle(
-                              color: Colors.black,
-                              fontSize: 18,
-                            ),
-                          ),
-                        ],
-                      ),
-                    ],
+                        ),
+                      ],
+                    ),
                   ),
                 ),
               ),
             ),
           ),
+          Padding(
+            padding: const EdgeInsets.only(right: 170),
+            child: ElevatedButton(
+                onPressed: () => solicitar(),
+                style: ButtonStyle(
+                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                        RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(18.0),
+                ))),
+                child: const Text('Solicitar Viagem',
+                    style: TextStyle(
+                      fontSize: 22,
+                    ))),
+          ),
         ]),
       ),
     );
   }
+    void info() {
+    Navigator.push(
+      context, MaterialPageRoute(builder: (context) => const HomePage()));
+}
+  void solicitar() {
+    Navigator.push(
+      context, MaterialPageRoute(builder: (context) => const HomePage()));
+}
 }
